47af74f8e2ff5017c92853fe9714e937dcfcfef1
JonathanLemes <JonathanLemes@unifei.edu.br>
2020-07-28 17:13:30 -0300

    aula_1_componentes_extras

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css'
+import Button from '../Button'
 
 function Menu() {
     return (
         <nav className="Menu">
             <a href="/">
-                <img className="Logo" src={Logo} alt="Logo da AluraFlix" />
+                <img className="Logo" src={Logo} alt="Dunkflix" />
             </a>
-            <a className="ButtonLink" href="/">Novo vídeo</a>
+            <Button as="a" className="ButtonLink" href="/">
+                Novo vídeo
+            </Button>
         </nav>
     )
 }
